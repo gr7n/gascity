05976480f2fb3bbebba1e9a387b06c48eee25d6e
bryceputman <bputm@BP.localdomain>
2026-07-25 17:59:51 -0400

    test: reconcile runtime stack resource census

--- internal/testpolicy/resourcecensus/census.go
+++ internal/testpolicy/resourcecensus/census.go
@@ -123,7 +123,7 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeAll,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   535,
+			BaselineCalls:   538,
 			BaselineFiles:   163,
 			ReportedCalls:   495,
 			ReportedFiles:   135,
@@ -164,7 +164,7 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeUntagged,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   396,
+			BaselineCalls:   398,
 			BaselineFiles:   112,
 			ReportedCalls:   380,
 			ReportedFiles:   98,
@@ -442,7 +442,7 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeUntagged,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   391,
+			BaselineCalls:   393,
 			BaselineFiles:   109,
 			ReportedCalls:   394,
 			ReportedFiles:   105,
